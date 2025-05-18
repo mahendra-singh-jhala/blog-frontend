8122c1a1b2307cba0c3487bcb762b0358e96b15c
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-05-18 09:59:55 +0530

    fix deployment issue

--- src/component/auth/Login.js
+++ src/component/auth/Login.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import toast from 'react-hot-toast';
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const navigate = useNavigate()
 
     // function to handle form submission
     const handleSubmit = async (e) => {
